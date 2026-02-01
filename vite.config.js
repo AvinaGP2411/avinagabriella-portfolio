e052b24b3f39d853a0cddbde9f93e743a4fda3ee
AvinaGP2411 <avinaworkspace@gmail.com>
2026-02-01 15:19:10 +0700

    fix: vite base & tailwind config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "./",   // PENTING! supaya build bisa jalan di Netlify/GitHub Pages
+  base: "./", // PENTING! supaya build bisa jalan di Netlify/GitHub Pages
   plugins: [react(), tailwindcss()],
   server: {
     open: true,
-    host: false,
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
   },
 });
