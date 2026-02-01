fd93f83b2bc5b44408c7bf851dbbb92c9ff1cb2b
AvinaGP2411 <avinaworkspace@gmail.com>
2026-02-01 14:36:10 +0700

    fix: vite base path & add netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  // const env = loadEnv(mode, process.cwd(), ""); // bisa dihapus kalau tidak pakai env
-  return {
-    base: "./", // penting supaya build bisa jalan di Netlify
-    plugins: [react(), tailwindcss()],
-    server: {
-      open: true,
-      host: false,
-      proxy: {
-        "/api": {
-          target: "http://localhost:5000",
-          changeOrigin: true,
-          secure: true,
-        },
+export default defineConfig({
+  base: "./",   // PENTING! supaya build bisa jalan di Netlify/GitHub Pages
+  plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+    host: false,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: true,
       },
     },
-  };
+  },
 });
